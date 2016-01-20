252ccc3f63bc2f5ca0584591cb57c89b166dcb23
hadasnadan <nadan.hadas@gmail.com>
2016-01-20 10:21:01 +0200

    styles

--- includes/controller.js
+++ includes/controller.js
@@ -17,30 +17,64 @@ var questions = [
 var values;
 var user_prop;
 
+// resize height mechanism
+/*var setHeights = function() {
+	console.log("set height");
+	windowWidth = $(window).innerWidth();
+	//console.log(windowWidth);
+	if(windowWidth > 851) {
+		windowHeight = $(window).innerHeight();
+		//console.log(windowHeight);
+		//$('#resultsContainer').css('top', (windowHeight/10)+'px');
+		//$('#logoContainerCalc').css('top', (windowHeight/3)+'px');
+		$('#questionContainer').css('height', (windowHeight/4)+'px');
+		$('#logoContainerCalc').css('height', (windowHeight/6)+'px');
+	} else {
+		$(window).off("resize", setHeights);
+		$(window).resize(checkWidth);
+	}
+};
+
+var checkWidth = function() {
+	windowWidth = $(window).innerWidth();
+	console.log("check width");
+	if(windowWidth > 850) {
+		$(window).off("resize", checkWidth);
+		$(window).resize(setHeights);
+	}
+};*/
+
 var loadCalculator = function() {
 	var assumption_val = commaSeparateNumber(getUserProperty("assumption_pens"));
 	$('#assumpTxt').html(assumption_val);
 	var gender = getUserProperty("gender");
 	$('document').ready(function() {
+		var queryDict = {};
+		location.search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1];});
+		var isBack = (queryDict['back'] == "true");
 		$.getJSON("includes/data.json", function(data) {
 			calculator_params = data["calculator_scales"];
 			initParams(calculator_params);
 			values = new Array(calculator_params.length);
 			for (var i = 0; i < calculator_params.length; i++) {
 				var id = "range"+i;
-				var sHTML = '<section class="slider"><section class="questionContainer"><section class="calcQues">';
+				var sHTML = '<section class="slider"><section class="questionContainer"><p>';
 				if(gender == Gender.MALE) {
 					sHTML += calculator_params[i]["male_q"];
 				} else if (gender == Gender.FEMALE) {
 					sHTML += calculator_params[i]["female_q"];
 				}
-				sHTML += '</section></section><section class="expContainer"><section class="calcExp">';
+				sHTML += '</p></section><section class="expContainer"><p>';
 				sHTML += calculator_params[i]["exp"];
-				sHTML += '</section></section><input type="text" id="'+id+'" value="" name="range"></section><section class="hrLine"></section>';
+				sHTML += '</p></section><input type="text" id="'+id+'" value="" name="range"></section><section class="hrLine"></section>';
 				var slider = $(sHTML);
-				$("#sliders").append(slider);
-				
+				$("#sliders").append(slider);				
+				if(isBack) {
+					calculator_params[i]["default"] = getUserProperty(id);
+					console.log(id + ": "+calculator_params[i]["default"]);
+				}
 				if(calculator_params[i]["is_fixed"]) {
+					calculator_params[i]["default"] = translateDefault(i, calculator_params[i]["default"]);
 					createFixedSlider(i, calculator_params[i]);
 				} else {
 					createSlider(i, calculator_params[i]);	
@@ -49,26 +83,69 @@ var loadCalculator = function() {
 			var btn = '<section class="continueBtn"><section class="continueBtnTxt">עבור לתוצאות המחשבון</section></section>';
 			$("#sliders").append(btn);
 			$('.continueBtn').click(function() {
+				if(!isBack) {
+					
+				}
 			   	setUserProperty("result_pens", pens_finite);
+			   	saveUserResults();
+			   	if(!isBack) {
+					var user_pension_val = $('#pensionTxt').text();
+				   	var user_pension_result = parseInt(user_pension_val.replace(',', ''));
+				   	var salary = values[0];
+				   	var dbgender;
+				   	(getUserProperty("gender") == Gender.MALE) ? dbgender = "Male" : dbgender = "Female";
+				   	var duration = Number(values[1]) + Number(getUserProperty("pre_years"));
+				   	var pens_perc = values[3];
+				   	var emp_perc = values[4];
+				   	var mng_mon = values[7];
+				   	var mng_year = values[8];
+				   	var data = {'result':user_pension_result, 'salary':salary, 'gender':dbgender, 'duration':duration, 'pens_perc':pens_perc,
+				        		'emp_perc':emp_perc, 'mng_mon':mng_mon, 'mng_year':mng_year};
+				    console.log(data);
+	     			$.post("processDB.php", data, function(response,status){ // Required Callback Function
+						//"response" receives - whatever written in echo of above PHP script.
+						alert("*----Received Data----*\n\nResponse : " + response+"\n\nStatus : " + status);
+						//window.location.href = 'finish.php';
+					});	
+				}
 				window.location.href = 'finish.html';
 			});
 			getParams();
 			calculate();
 		});
-		/* $(document).scroll(function() {
-			  var y = $(this).scrollTop();
-			  if (y > 500) {
-			    $('.bottomFinish').fadeIn('fast');
-			    $('.bottomFinish').click(function() {
-			    	setUserProperty("result_pens", pens_finite);
-					window.location.href = 'finish.html';
-				});
-			  }
-		}); */
 	});
 };
 
+var translateDefault = function(i, def) {
+	switch(i) {
+		case 3:
+			if (Number(def) == 5.5)
+				return 0;
+			else if(Number(def) == 6)
+				return 1;
+			else return 2;
+			break;
+		case 4:
+			if (Number(def) == 11)
+				return 0;
+			else if(Number(def) == 12)
+				return 1;
+			else return 2;
+			break;
+		default:
+			return 0;
+	}
+};
+
+var saveUserResults = function() {
+	for (var i = 0; i < values.length; i++) {
+		var id = "range"+i;
+		setUserProperty(id, values[i]);
+	}
+};
+
 var createFixedSlider = function(id, scale) {
+	console.log(id + ": "+scale["default"]);
 	$("#range"+id).ionRangeSlider({
     	keyboard: true,
         min: scale["min_val"],
@@ -101,10 +178,12 @@ var createFixedSlider = function(id, scale) {
 };
 
 var createSlider = function(id, scale) {
+	console.log(id + ": "+scale["default"]);
 	var from = scale["default"];
-	if(id == 0) {
-		from = salary;
-	} else if (id == 9) {
+	// if(id == 0) {
+		// from = salary;
+	// } else 
+	if (id == 9) {
 		from = savings;
 	}
 	$("#range"+id).ionRangeSlider({
@@ -362,7 +441,12 @@ var setScreen = function() {
 };
 
 var generateResult = function() {
-	var diff = Number(Number(getUserProperty("assumption_pens")) - Number(getUserProperty("result_pens")));
+	var asmp = Number(getUserProperty("assumption_pens"));
+	var pens = Number(getUserProperty("result_pens"));
+	$('#assumpTxt').html(commaSeparateNumber(asmp));
+	$('#pensionTxt').html(commaSeparateNumber(pens));
+	resizeResultContainer(pens);
+	var diff = asmp - pens;
 	$('document').ready(function() {
 		$.getJSON("includes/data.json", function(data) {
 			var resp_txt;
@@ -386,9 +470,125 @@ var generateResult = function() {
 			$('#result_text_pens').html(getUserProperty("result_pens"));
 			$('#result_text_assump').html(getUserProperty("assumption_pens"));
 			$('#center_txt').html(resp_txt);
+			$('.continueBtn').click(function() {
+				window.location.href="calculator.html?back=true";
+			});
+			attachSocialPlugins();
 		});
 	});
 };
+
+
+var attachSocialPlugins = function() {
+	// Share via whatsapp
+	$(document).on("click", '.whatsapp', function() {
+		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+			var text = $(this).attr("data-text");
+			var url = $(this).attr("data-link");
+			var message = encodeURIComponent(text) + " - " + encodeURIComponent(url);
+			var whatsapp_url = "whatsapp://send?text=" + message;
+			window.location.href = whatsapp_url;
+		} else {
+			alert("Please use an Mobile Device to Share this Article");
+		}
+	});
+	
+	// Share via facebook
+	$('.facebook').snsShare('בדקו את הפנסיה שלכם במחשבון הפנסיה של כלכליסט', 'http://shenkar.html5-book.co.il/2015-2016/lab/dev_13/');
+	
+	// Share via email
+	$(document).on("click", '.email', function() {
+		window.location.href = "mailto:?subject=כשתהיו בני 67&body=בדקו את הפנסיה שלכם במחשבון הפנסיה של כלכליסט http://shenkar.html5-book.co.il/2015-2016/lab/dev_13/";
+	});
+	
+	// download excell
+	$(document).on("click", '.excell', function() {
+		// **************** get user parameters!!!! ************************
+		var data = generateData();
+		//var data = [{"Vehicle":"BMW","Date":"30, Jul 2013 09:24 AM","Location":"Hauz Khas, Enclave, New Delhi, Delhi, India","Speed":42},{"Vehicle":"Honda CBR","Date":"30, Jul 2013 12:00 AM","Location":"Military Road,  West Bengal 734013,  India","Speed":0},{"Vehicle":"Supra","Date":"30, Jul 2013 07:53 AM","Location":"Sec-45, St. Angel's School, Gurgaon, Haryana, India","Speed":58},{"Vehicle":"Land Cruiser","Date":"30, Jul 2013 09:35 AM","Location":"DLF Phase I, Marble Market, Gurgaon, Haryana, India","Speed":83},{"Vehicle":"Suzuki Swift","Date":"30, Jul 2013 12:02 AM","Location":"Behind Central Bank RO, Ram Krishna Rd by-lane, Siliguri, West Bengal, India","Speed":0},{"Vehicle":"Honda Civic","Date":"30, Jul 2013 12:00 AM","Location":"Behind Central Bank RO, Ram Krishna Rd by-lane, Siliguri, West Bengal, India","Speed":0},{"Vehicle":"Honda Accord","Date":"30, Jul 2013 11:05 AM","Location":"DLF Phase IV, Super Mart 1, Gurgaon, Haryana, India","Speed":71}];
+		// *****************************************************************
+        JSONToCSVConvertor(data, "", true);
+    });
+}; 
+
+var generateData = function() {
+	var data = new Array(1);
+	data[0] = {};
+	data[0]["Salary"] = getUserProperty("range0");
+	data[0]["Years_of_savings"] = getUserProperty("range1"); 
+	data[0]["Future_salary"] = getUserProperty("range2"); 
+	data[0]["Pension_percentage"] = Number(getUserProperty("range3"))+Number(getUserProperty("range4"));
+	data[0]["Annual_return"] = getUserProperty("range5"); 
+	data[0]["Severance_pay_withdraw"] = getUserProperty("range6"); 
+	data[0]["Management_fee_per_month"] = getUserProperty("range7"); 
+	data[0]["Management_fee_per_year"] = getUserProperty("range8"); 
+	data[0]["Savings"] = getUserProperty("range9");  
+	data[0]["Pension_assumptiom"] = getUserProperty("assumption_pens"); 
+	data[0]["Future_pension"] = getUserProperty("result_pens");  
+	return data;
+};
+
+function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
+    // If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+    var CSV = '';    
+    // Set Report title in first row or line
+    
+    CSV += ReportTitle + '\r\n\n';
+
+    //This condition will generate the Label/Header
+    if (ShowLabel) {
+        var row = "";
+        //This loop will extract the label from 1st index of on array
+        for (var index in arrData[0]) {
+            //Now convert each value to string and comma-seprated
+            row += index + ',';
+        }
+        row = row.slice(0, -1);
+        //append Label row with line break
+        CSV += row + '\r\n';
+    }
+    
+    console.log(CSV);
+    
+    //1st loop is to extract each row
+    for (var i = 0; i < arrData.length; i++) {
+        var row = "";    
+        //2nd loop will extract each column and convert it in string comma-seprated
+        for (var index in arrData[i]) {
+            row += '"' + arrData[i][index] + '",';
+        }
+        row.slice(0, row.length - 1);
+        //add a line break after each row
+        CSV += row + '\r\n';
+    }
+
+    if (CSV == '') {        
+        alert("Invalid data");
+        return;
+    }   
+    
+    //Generate a file name
+    var fileName = "my_pension";
+    //this will remove the blank-spaces from the title and replace it with an underscore
+    fileName += ReportTitle.replace(/ /g,"_");   
+    
+    //Initialize file format you want csv or xls
+    var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+    
+    //this trick will generate a temp <a /> tag
+    var link = document.createElement("a");    
+    link.href = uri;
+    
+    //set the visibility hidden so it will not effect on your web-layout
+    link.style = "visibility:hidden";
+    link.download = fileName + ".csv";
+    
+    //this part will append the anchor tag and remove it after automatic click
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
 
 var resizeResultContainer = function(pension) {
 	var assumption_val = commaSeparateNumber(getUserProperty("assumption_pens"));
@@ -425,3 +625,14 @@ var commaSeparateNumber = function(val){
 };
 
 //resize text function!
+
+/* $(document).scroll(function() {
+			  var y = $(this).scrollTop();
+			  if (y > 500) {
+			    $('.bottomFinish').fadeIn('fast');
+			    $('.bottomFinish').click(function() {
+			    	setUserProperty("result_pens", pens_finite);
+					window.location.href = 'finish.html';
+				});
+			  }
+		}); */
---
+++ menu.php
@@ -0,0 +1,22 @@
+<?php 
+	$menu = '<div class="navbar-header">'.
+          '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">'.
+           ' <span class="sr-only">Toggle navigation</span>'.
+            '<span class="icon-bar"></span>'.
+            '<span class="icon-bar"></span>'.
+            '<span class="icon-bar"></span>'.
+          '</button>'.
+        '</div>'.
+        '<div id="navbar" class="collapse navbar-collapse">'.
+         ' <ul class="nav navbar-nav">'.
+           '  <li><a href="allStatistics.php">All  <i class="glyphicon glyphicon-object-align-bottom" aria-hidden="true"></i></a></li>'.
+           '  <li><a href="logout.php">Gender  <i class="glyphicon glyphicon-object-align-bottom" aria-hidden="true"></i></a></li>'.
+           '  <li><a href="logout.php">Salary  <i class="glyphicon glyphicon-object-align-bottom" aria-hidden="true"></i></a></li>'.
+           '  <li><a href="logout.php">Duration  <i class="glyphicon glyphicon-object-align-bottom" aria-hidden="true"></i></a></li>'.
+           '  <li><a href="logout.php">Pension Percentage  <i class="glyphicon glyphicon-object-align-bottom" aria-hidden="true"></i></a></li>'.
+           '  <li><a href="logout.php">Pension by Employee <i class="glyphicon glyphicon-object-align-bottom" aria-hidden="true"></i></a></li>'.
+           '  <li><a href="logout.php">Management per Month <i class="glyphicon glyphicon-object-align-bottom" aria-hidden="true"></i></a></li>'.
+           '  <li><a href="logout.php">Management per Year <i class="glyphicon glyphicon-object-align-bottom" aria-hidden="true"></i></a></li>'.
+          '</ul>'.
+        '</div>';
+?>
